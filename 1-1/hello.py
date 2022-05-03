34064fd3d8897d121fade0aba600db1248e726bc
sukarus0 <sukarus@gamil.com>
2022-05-03 01:51:43 +0000

    Done 1.4.1 second example

--- 1-1/hello.py
+++ 1-1/hello.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/users/<username>')
+def get_user(username):
+    return username
+
+@app.route('/posts/<int:post_id>')
+def get_post(post_id):
+    return str(post_id)
+
+@app.route('/uuid/<uuid:uuid>')
+def get_uuid(uuid):
+    return str(uuid)
